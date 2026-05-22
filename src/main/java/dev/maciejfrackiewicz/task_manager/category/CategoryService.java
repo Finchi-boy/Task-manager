6e4fd6b01a305fbcdbce08d66862a418490a4529
Finchii <284302@student.pwr.edu.pl>
2026-05-22 08:41:38 +0200

    feat: add service for Category

---
+++ src/main/java/dev/maciejfrackiewicz/task_manager/category/CategoryService.java
@@ -0,0 +1,34 @@
+package dev.maciejfrackiewicz.task_manager.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public Category addCategory(Category category)
+    {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> getCategory(UUID id)
+    {
+        return categoryRepository.findById(id);
+    }
+
+    public Category updateCategory(Category category)
+    {
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(UUID id)
+    {
+        categoryRepository.deleteById(id);
+    }
+
+}

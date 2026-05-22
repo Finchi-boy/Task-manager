a4f5da6519f54407e573ee53c4dee32da3ef2ecf
Finchii <284302@student.pwr.edu.pl>
2026-05-22 07:34:13 +0200

    feat: add Task entity with TaskStatus enum

---
+++ src/main/java/dev/maciejfrackiewicz/task_manager/task/Task.java
@@ -0,0 +1,32 @@
+package dev.maciejfrackiewicz.task_manager.task;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="tasks")
+
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID userId;
+    private UUID categoryId;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    private LocalDateTime deadline;
+    private LocalDateTime createdAt;
+
+}

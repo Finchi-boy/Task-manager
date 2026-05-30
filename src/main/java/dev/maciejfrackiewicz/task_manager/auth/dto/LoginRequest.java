dfa4bc4f16c3fe79a82acf0c64deec6bc0be98c0
Finchii <284302@student.pwr.edu.pl>
2026-05-30 09:19:48 +0200

    feat: add validation constraints for Auth DTOs

--- src/main/java/dev/maciejfrackiewicz/task_manager/auth/dto/LoginRequest.java
+++ src/main/java/dev/maciejfrackiewicz/task_manager/auth/dto/LoginRequest.java
@@ -1,7 +1,15 @@
 package dev.maciejfrackiewicz.task_manager.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequest(
+        @Email
+        @NotBlank
         String email,
+        @NotBlank
+        @Size(min = 8)
         String password
 ) {
 }
--- src/main/java/dev/maciejfrackiewicz/task_manager/auth/dto/RegisterRequest.java
+++ src/main/java/dev/maciejfrackiewicz/task_manager/auth/dto/RegisterRequest.java
@@ -1,8 +1,17 @@
 package dev.maciejfrackiewicz.task_manager.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
+        @Email
+        @NotBlank
         String email,
+        @NotBlank
         String username,
+        @NotBlank
+        @Size(min = 8)
         String password
 ) {
 

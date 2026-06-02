9ebfa0ae31ff5fba19a90cb6e53a39274279d55f
Finchii <284302@student.pwr.edu.pl>
2026-06-02 06:45:39 +0200

    feat: add getting all user's tasks

--- src/main/java/dev/maciejfrackiewicz/task_manager/task/TaskRepository.java
+++ src/main/java/dev/maciejfrackiewicz/task_manager/task/TaskRepository.java
@@ -2,7 +2,10 @@ package dev.maciejfrackiewicz.task_manager.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByUserId(UUID userId);
 }
